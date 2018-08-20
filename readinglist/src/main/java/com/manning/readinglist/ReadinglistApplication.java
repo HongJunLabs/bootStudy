383b3098d2219418c5471f595b8ab99909217bb9
HongJunLabs <42328518+HongJunLabs@users.noreply.github.com>
2018-08-20 13:24:30 +0900

    develop branch 시작

--- readinglist/src/main/java/com/manning/readinglist/ReadinglistApplication.java
+++ readinglist/src/main/java/com/manning/readinglist/ReadinglistApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReadinglistApplication {
-
+	
+	//-- 개발시작코드
 	public static void main(String[] args) {
 		SpringApplication.run(ReadinglistApplication.class, args);
 	}
