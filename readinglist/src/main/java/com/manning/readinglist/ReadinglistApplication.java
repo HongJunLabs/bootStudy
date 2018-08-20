61a7a561557becb442a191318dcf8070ef866f14
HongJunLabs <42328518+HongJunLabs@users.noreply.github.com>
2018-08-20 15:48:10 +0900

    [김홍준][수정][스프링부트 샘플 주석 추가]

--- readinglist/src/main/java/com/manning/readinglist/ReadinglistApplication.java
+++ readinglist/src/main/java/com/manning/readinglist/ReadinglistApplication.java
@@ -3,11 +3,17 @@ package com.manning.readinglist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//-- 컴포넌트 검색과 자동 구성 활성화 (@@SpringBootApplication 은 아래 설명하는 애너테이션의 기능이 합쳐졌다.)
+//-- @Configuration : 이 애너테이션이 붙은 클래스를 스프링의 자바 기반 구성 클래스로 지정(설정 용도)
+//-- @ComponentScan : 컴포넌트 검색 기능을 활성화해서 웹 컨트롤러 클래스나 다른 컴포넌트 클래스들을 자동으로 검색하여 스프링 애플리케이션 컨텍스트에 빈으로 등록 시킨다.
+//-- @EnableAutoConfiguration : 스프링 부트의 자동 구성을 할 수 있는 애너테이션
+
 @SpringBootApplication
 public class ReadinglistApplication {
 	
 	//-- 개발시작코드
 	public static void main(String[] args) {
+		//-- 애플리케이션 부트스트랩
 		SpringApplication.run(ReadinglistApplication.class, args);
 	}
 }
--- readinglist/src/test/java/com/manning/readinglist/ReadinglistApplicationTests.java
+++ readinglist/src/test/java/com/manning/readinglist/ReadinglistApplicationTests.java
@@ -6,6 +6,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+//-- 테스트에서 사용할 bean을 지장한다. ex : @SpringBootTest(classes = {ClassName1.class, ClassName2.class})
+//-- @Configuration 어노테이션을 사용하는 클래스가 있다면 내부에서 @Bean 어노테이션을 통해서 생성되는 빈도 모두 등록 된다.
+//-- 만일 classes 속성을 통해서 클래스를 지정하지 않으면 애플리케이션 상에 정의된 모든 빈을 생성한다.
+//-- 아래는 Example
+/*
+ *  @RunWith(SpringRunner.class)
+	@SpringBootTest(classes = {ArticleServiceImpl.class, CommonConfig.class})
+	public class SomeClassTest {
+		// Service로서 등록된 빈
+		@Autowired
+		private ArticleServiceImpl articleServiceImpl;
+		// CommonConfig에서 생성되는 빈
+		@Autowired
+		private RestTemplate restTemplate;
+	}
+ * 
+ * 
+ * */
+
 @SpringBootTest
 public class ReadinglistApplicationTests {
 
